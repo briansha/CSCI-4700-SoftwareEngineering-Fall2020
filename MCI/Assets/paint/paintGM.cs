82528bac9f138f779dac339ee3c97cc3fb17eb73
RujH <rujhaan01@gmail.com>
2020-10-06 14:05:28 -0500

    added version 1 free draw

---
+++ MCI/Assets/paint/paintGM.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paintGM : MonoBehaviour
+{ 
+    public Transform baseDot;
+    public KeyCode mouseLeft;
+    public static string toolType;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        
+
+        if (Input.GetKey(mouseLeft))
+        {
+            Instantiate(baseDot, objPosition, baseDot.rotation);
+        }
+    }
+    
+}
