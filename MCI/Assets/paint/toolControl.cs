bdf25eb3a03df94aca76bc9b9d5c61c3d3a76d6c
RujH <rujhaan01@gmail.com>
2020-10-07 23:56:11 -0500

    Added free draw task

---
+++ MCI/Assets/paint/toolControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toolControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        if (gameObject.name == "eraser")
+        {
+            paintGM.toolType = "eraser";
+            //Debug.Log("eraser selected");
+        }
+        if (gameObject.name == "pencil")
+        {
+            paintGM.toolType = "pencil";
+            //Debug.Log("eraser selected");
+        }
+    }
+}
