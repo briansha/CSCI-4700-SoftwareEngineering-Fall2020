3a10997a216061097ca17f198568dad5007cb0f7
RujH <rujhaan01@gmail.com>
2020-11-11 14:03:19 -0600

    updated free draw task

---
+++ MCI/Assets/Scenes/FreeDraw/paint/dotControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class dotControl : MonoBehaviour
+{
+
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().color=paintGM.currentColor;
+        GetComponent<Transform>().localScale = new Vector2(paintGM.currentScale, paintGM.currentScale);
+    }
+    void OnMouseOver()
+    {
+        if (paintGM.toolType == "eraser")
+                {
+                    Destroy(gameObject);
+                }
+    }
+
+
+        
+    
+}
---
+++ MCI/Assets/Scenes/FreeDraw/paint/toolControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toolControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        if (gameObject.name == "eraser")
+        {
+            paintGM.toolType = "eraser";
+            
+        }
+
+
+        if (gameObject.name == "pencil")
+        {
+            paintGM.toolType = "pencil";
+            
+        }
+        if (gameObject.name == "pencilSizeUp")
+        {
+            paintGM.currentScale += 4;
+
+        }
+        if (gameObject.name == "pencilSizeDown")
+        {
+            paintGM.currentScale -= 4;
+
+        }
+    }
+}
