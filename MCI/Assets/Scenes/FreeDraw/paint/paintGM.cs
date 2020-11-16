e4168a23db2e40deccb9ee21eda3fc49ae62c801
RujH <rujhaan01@gmail.com>
2020-11-16 16:06:12 -0600

    added eraser to free draw task

--- MCI/Assets/Scenes/FreeDraw/paint/paintGM.cs
+++ MCI/Assets/Scenes/FreeDraw/paint/paintGM.cs
@@ -9,7 +9,7 @@ public class paintGM : MonoBehaviour
     public static string toolType;
     public static bool cavDraw = false;
     public static Color currentColor;
-
+    public static Color imageColor;
     public Vector2 prevVec = new Vector2(999,999);
     public static int currentScale = 4;
     private Texture2D texture ;
@@ -17,24 +17,28 @@ public class paintGM : MonoBehaviour
 
     void Start()
     {
-        texture = new Texture2D(575, 375);
+        texture = new Texture2D(500, 250);
+        imageColor = texture.GetPixel(500, 250);
     }
 
     void doTheArt()
     {
       
-        if (toolType == "pencil" )
+        if (toolType == "pencil" ||toolType=="eraser" )
         {
             
             if (Input.GetKey(mouseLeft)&& cavDraw)
             {
-               int x = (int)Input.mousePosition.x;
-               int y= (int)Input.mousePosition.y;
-               for (int i =0; i<currentScale; i++)
+                int x = (int)Input.mousePosition.x;
+                int y = (int)Input.mousePosition.y;
+                /*                Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                                int x =(int) worldPoint.x;
+                                 int y = (int)worldPoint.y;*/
+                for (int i =0; i<currentScale; i++)
                 {
                     for (int j = 0; j < currentScale; j++)
                     {
-                        texture.SetPixel(x-28+i,y-108+j,currentColor);
+                        texture.SetPixel(x-25+i,y-105+j,currentColor);
                     }
                 }
                if (prevVec.x != 999)
@@ -45,26 +49,22 @@ public class paintGM : MonoBehaviour
                         for (int j = 0; j < currentScale; j++)
                         {
                             //texture.SetPixel(x - 28 + i, y - 108 + j, currentColor);
-                            DrawLine(new Vector2(prevVec.x+i, prevVec.y+j), new Vector2(x-28+i, y-108+j), currentColor);
+                            DrawLine(new Vector2(prevVec.x, prevVec.y), new Vector2(x-25+i, y-105+j), currentColor);
                         }
                     }
                     
-
                 }
-                prevVec = new Vector2(x - 28, y - 108);
+                prevVec = new Vector2(x-25, y-105);
 
 
                texture.Apply();
-               thing.GetComponent<Image>().sprite = Sprite.Create(texture, new Rect(0,0, 575,375), new Vector2 (0.5f,0.5f));
+               thing.GetComponent<Image>().sprite = Sprite.Create(texture, new Rect(0,0, 500,250), new Vector2 (0.5f,0.5f));
 
             }
           
 
         }
-        if (toolType == "eraser")
-        {
- 
-        }
+
 
     }
 
