c9dad563b0c55e0f8efb92705d2aa6e078770076
jmc2ei <61247183+jmc2ei@users.noreply.github.com>
2020-11-19 02:34:27 -0600

    Updated all transtions
    Updated all transitions and animations, also added escape key function

--- MCI/Assets/Scenes/MainMenu.cs
+++ MCI/Assets/Scenes/MainMenu.cs
@@ -5,8 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+  public Animator transition;
+  public float transitionTime = 1f;
+
+    // Update is called once per frame
 	public void PlayGame()
 	{
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	  StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+	}
+	IEnumerator LoadLevel(int levelindex)
+	{
+			//play animation
+			transition.SetTrigger("Start");
+			//wait
+			yield return new WaitForSeconds(transitionTime);
+			//play scene
+			SceneManager.LoadScene(levelindex);
 	}
 }
---
+++ MCI/Assets/TaskOneButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TaskOneButton : MonoBehaviour
+{
+  public Animator transition;
+  public float transitionTime = 1f;
+
+    // Update is called once per frame
+    public void changeScene(string sceneName)
+    {
+StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    IEnumerator LoadLevel(int levelindex)
+  	{
+  			//play animation
+  			transition.SetTrigger("Start");
+  			//wait
+  			yield return new WaitForSeconds(transitionTime);
+  			//play scene
+  			SceneManager.LoadScene(levelindex);
+  	}
+  }
